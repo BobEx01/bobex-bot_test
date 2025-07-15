5cc90558ceff3e552123bfa01912370629fb65b0
BobEx01 <boburi9000@mail.ru>
2025-07-16 03:01:21 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
-TOKEN = "6990781325:AAGs0UAvQ6PPgSok3xAi7QIP4Pf7cdB2yCA"  # Bot tokeningiz
-ADMIN_ID = 8080091052  # Admin ID
+TOKEN = '7131667480:AAGVdA-3tLsxZ-3RL7GjDWYhOCx3vRwWT6Y'
+ADMIN_ID = 8080091052
 
-PREMIUM_PRICE = 10000  # Premium e'lon narxi (so'm)
-NUMBER_ACCESS_PRICE = 28000  # Telefon raqamini olish narxi (so'm)
-
-DB_PATH = 'bobex.db'  # Ma'lumotlar bazasi fayli
+CARD_NUMBER = "8600123456789012"  
+CARD_OWNER = "Muhamamdbobu.A
