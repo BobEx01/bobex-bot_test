c797fee30a7089c5357fc0cf4bd97159da29f06d
BobEx01 <boburi9000@mail.ru>
2025-07-16 02:52:22 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
+TOKEN = "6990781325:AAGs0UAvQ6PPgSok3xAi7QIP4Pf7cdB2yCA"  # Bot tokeningiz
+ADMIN_ID = 8080091052  # Admin ID
 
-BOT_TOKEN = "7653469544:AAFH4xoRxu8-_nWy0CR1gXA1Nkv1txt3gqc"
-ADMIN_IDS = [8080091052]
-ADMIN_USERNAMES = ['bobex_uz']
-REFERRAL_BONUS = 2000
-JOIN_BONUS = 50000
-CARD_NUMBER = "5614 6822 1820 6250"
-CARD_OWNER = "Muhammadbobur.A"
+PREMIUM_PRICE = 10000  # Premium e'lon narxi (so'm)
+NUMBER_ACCESS_PRICE = 28000  # Telefon raqamini olish narxi (so'm)
+
+DB_PATH = 'bobex.db'  # Ma'lumotlar bazasi fayli
