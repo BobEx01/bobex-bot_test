80ddeed609f3353fd788caa8c2c137ea6e7d18bc
BobEx01 <boburi9000@mail.ru>
2025-07-16 02:35:38 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,29 +1,29 @@
-
 import sqlite3
 
-class Database:
-    def __init__(self, db_name='bot.db'):
-        self.db_name = db_name
-        self._init_db()
+class DB:
+    def __init__(self, db_name='bobex.db'):
+        self.connection = sqlite3.connect(db_name, check_same_thread=False)
+        self.cursor = self.connection.cursor()
+        self.create_tables()
 
-    def _init_db(self):
-        with sqlite3.connect(self.db_name) as conn:
-            cursor = conn.cursor()
-            cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+    def create_tables(self):
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users (
                 user_id INTEGER PRIMARY KEY,
-                balance REAL DEFAULT 0,
-                referrer_id INTEGER
-            )''')
-            conn.commit()
+                username TEXT,
+                first_name TEXT,
+                last_name TEXT
+            )
+        ''')
+        self.connection.commit()
 
-    def add_user(self, user_id, referrer_id=None):
-        with sqlite3.connect(self.db_name) as conn:
-            cursor = conn.cursor()
-            cursor.execute('INSERT OR IGNORE INTO users (user_id, referrer_id) VALUES (?, ?)', (user_id, referrer_id))
-            conn.commit()
+    def add_user(self, user_id, username, first_name, last_name):
+        self.cursor.execute('''
+            INSERT OR IGNORE INTO users (user_id, username, first_name, last_name)
+            VALUES (?, ?, ?, ?)
+        ''', (user_id, username, first_name, last_name))
+        self.connection.commit()
 
-    def update_balance(self, user_id, amount):
-        with sqlite3.connect(self.db_name) as conn:
-            cursor = conn.cursor()
-            cursor.execute('UPDATE users SET balance = balance + ? WHERE user_id = ?', (amount, user_id))
-            conn.commit()
+    def get_users(self):
+        self.cursor.execute('SELECT * FROM users')
+        return self.cursor.fetchall()
